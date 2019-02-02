106c8a567c3fdf33ec58b32a1489d832e44291dd
Rober RM <haschell@gmail.com>
2019-02-02 18:58:51 +0100

    Examen practico Typescript

--- app.js
+++ app.js
@@ -1,9 +1,60 @@
-"use strict";
-// Un archivo simple de TypeScript
-function saludar(nombre) {
-    console.log("Hola " + nombre.toUpperCase());
-}
-var wolverine = {
-    nombre: "Logan"
+// Uso de Let y Const
+var nombre = "Ricardo Tapia";
+var edad = 23;
+var PERSONAJE = {
+    nombre: nombre,
+    edad: edad
 };
-saludar(wolverine.nombre);
+function validar(heroe) {
+    console.log("El nombre del heroe es: " + heroe.nombre);
+    console.log("Las artes marciales que usa son: " + heroe.artesMarciales);
+}
+var batman = {
+    nombre: "Bruno Díaz",
+    artesMarciales: ["Karate", "Aikido", "Wing Chun", "Jiu-Jitsu"]
+};
+validar(batman);
+// Convertir esta funcion a una funcion de flecha
+//function resultadoDoble( a, b ){
+//return (a + b) * 2
+//}
+var resultadoDobleF = function (a, b) { return (a + b) * 2; };
+var resultado = resultadoDobleF(1, 2);
+console.log("Resultado: " + resultado);
+// Función con parametros obligatorios, opcionales y por defecto
+// donde NOMBRE = obligatorio
+//       PODER  = opcional
+//       ARMA   = por defecto = "arco"
+function getAvenger(nombre, arma, poder) {
+    if (arma === void 0) { arma = "arco"; }
+    var mensaje;
+    if (poder) {
+        mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
+    }
+    else {
+        mensaje = nombre + " tiene un " + poder;
+    }
+    console.log("Mensaje: " + mensaje);
+}
+;
+getAvenger("Hawkeye", "ballesta", "punteria");
+// Cree una clase que permita manejar la siguiente estructura
+// La clase se debe de llamar rectangulo,
+// debe de tener dos propiedades:
+//   * base
+//   * altura
+// También un método que calcule el área  =  base * altura,
+// ese método debe de retornar un numero.
+var rectangulo = /** @class */ (function () {
+    function rectangulo() {
+        this.base = 0;
+        this.altura = 0;
+    }
+    return rectangulo;
+}());
+function calculaArea(base, altura) {
+    var resultado = base * altura;
+    return resultado;
+}
+var resul = calculaArea(5, 2);
+console.log("Resultado: " + resul);
--- app.ts
+++ app.ts
@@ -1,11 +1,77 @@
-// Un archivo simple de TypeScript
+// Uso de Let y Const
+const nombre = "Ricardo Tapia";
+const edad = 23;
+let PERSONAJE = {
+  nombre: nombre,
+  edad: edad
+};
+ 
+ 
+// Cree una interfaz que sirva para validar el siguiente objeto
 
-function saludar(nombre: string) {
-    console.log("Hola " + nombre.toUpperCase());
+interface HombreMurcielago{
+    nombre: string;
+    artesMarciales: string[];
 }
 
-var wolverine = {
-    nombre: "Logan"
+function validar(heroe: HombreMurcielago){
+    console.log("El nombre del heroe es: " +heroe.nombre);
+    console.log("Las artes marciales que usa son: " +heroe.artesMarciales);
 }
 
-saludar(wolverine.nombre);
+let batman = {
+  nombre: "Bruno Díaz",
+  artesMarciales: ["Karate","Aikido","Wing Chun","Jiu-Jitsu"]
+}
+validar(batman);
+
+ 
+ 
+// Convertir esta funcion a una funcion de flecha
+//function resultadoDoble( a, b ){
+  //return (a + b) * 2
+//}
+
+let resultadoDobleF = (a:number, b:number)  => (a+b)*2;
+let resultado = resultadoDobleF(1,2);
+console.log("Resultado: " +resultado);
+ 
+ 
+// Función con parametros obligatorios, opcionales y por defecto
+// donde NOMBRE = obligatorio
+//       PODER  = opcional
+//       ARMA   = por defecto = "arco"
+function getAvenger( nombre:string, arma:string ="arco", poder?:string ){
+  var mensaje;
+  if( poder ){
+     mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
+  }else{
+     mensaje = nombre + " tiene un " + poder
+  }
+  console.log("Mensaje: " +mensaje);
+};
+
+getAvenger("Hawkeye", "ballesta","punteria" );
+ 
+ 
+// Cree una clase que permita manejar la siguiente estructura
+// La clase se debe de llamar rectangulo,
+// debe de tener dos propiedades:
+//   * base
+//   * altura
+// También un método que calcule el área  =  base * altura,
+// ese método debe de retornar un numero.
+
+class rectangulo{
+    base:number=0;
+    altura:number=0;
+
+    constructor(){   }
+}
+
+function calculaArea(base: number, altura:number){
+    let resultado = base * altura;
+    return resultado;
+}
+let resul = calculaArea(5,2);
+console.log("Resultado: " +resul);
