1fb7f9d04728713bd5a55f612f5a315e984ec822
Rober RM <haschell@gmail.com>
2019-02-04 21:33:54 +0100

    Seccion 3 - antes de ngif y ngfor - ultima leccion

--- 01-hola-mundo/src/app/app.module.ts
+++ 01-hola-mundo/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 
 import { AppComponent } from './app.component';
-import { componentFactoryName } from '@angular/compiler';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
