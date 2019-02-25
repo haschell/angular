b2a3ae471aa2a8d51fbdf954c965edebd1a506bb
Roberto Ruiz <haschell@gmail.com>
2019-02-25 22:16:15 +0100

    Antes de empezar clase 104

--- 04-spotiapp/src/app/services/spotify.service.ts
+++ 04-spotiapp/src/app/services/spotify.service.ts
@@ -13,18 +13,28 @@ export class SpotifyService {
     const url = `https://api.spotify.com/v1/${query}`;
 
     const headers = new HttpHeaders ({
-      Authorization: 'Bearer BQC8zyFWkkPQyz7Cz-PjVvZalhhZUXlek0QlhYuSqcNxh5vi1K2FT7Lw5YpZKV1xZ0AawbEfAS6-NSB41ow'
+      Authorization: 'Bearer BQC-tSKxSllS_Qf2Syg-BH8aPkrLvGNOnXawgfgFmX5lyynEWgS6QJW1y9hEsA8kzIN8_Y5N-hA_HwpVx0Y'
     });
     return this.http.get(url, {headers});
   }
 
   getNewReleases() {
-    return this.getQuery('browse/new-releases').pipe( map( data => data['albums'].items));
+    return this.getQuery('browse/new-releases').pipe( map( data => data[`albums`].items));
   }
 
-  getArtista(termino: string) {
-    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`).pipe( map( data => data['artists'].items));
+  getArtistas(termino: string) {
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`).pipe( map( data => data[`artists`].items));
   }
+
+  getArtista(id: string) {
+    return this.getQuery(`artists/${ id }`);
+    // .pipe (map(data => data['artist'].items));
+  }
+
+  getTopTracks(id: string) {
+    return this.getQuery(`artists/${ id }/top-tracks?country=es`).pipe (map(data => data[`tracks`]));
+  }
+
 }
 
 
