aa36cca4abd9a86c6c876d7e3d0dc70d6d7aa515
Roberto Ruiz <haschell@gmail.com>
2019-03-06 21:40:57 +0100

    Empezar leccion 118

--- 05-deseos/src/app/services/deseos.service.ts
+++ 05-deseos/src/app/services/deseos.service.ts
@@ -10,6 +10,9 @@ export class DeseosService {
 
 
   constructor() {
-    console.log('Servicio inicializado');
+    const lista1 = new Lista('Recolectar piedras del infinito');
+    const lista2 = new Lista('Heroes a desaparecer');
+
+    this.listas.push(lista1, lista2);
   }
 }
