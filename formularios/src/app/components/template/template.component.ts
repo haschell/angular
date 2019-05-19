1e36b4a0f8e8cc625af9dd34e2612c44f32065ba
Roberto AMD <haschell@gmail.com>
2019-05-19 22:02:46 +0200

    Empezar clase 172

--- formularios/src/app/components/template/template.component.ts
+++ formularios/src/app/components/template/template.component.ts
@@ -1,19 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+    .ng-invalid.ng-touched:not(form) {
+      border: 1px solid red;
+    }
+  `]
 })
 export class TemplateComponent implements OnInit {
+
+  usuario: Object = {
+    nombre: null,
+    apellido: null,
+    email: null
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  guardar() {
+  guardar(forma: NgForm) {
     console.log('Formulario posteado');
+    console.log("NgForm: ", forma);
+    console.log("Valor forma: ", forma.value);
+
+    console.log("USUARIO: ", this.usuario);
   }
 
 }
