87c6d4cc1bfa9a24172b6d62e9b1212fe78605b2
Roberto AMD <haschell@gmail.com>
2019-05-21 21:46:00 +0200

    Empezar clase 173 cuando arregle el HTML

--- formularios/src/app/components/template/template.component.ts
+++ formularios/src/app/components/template/template.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-template',
-  templateUrl: './template.component.html',
+  selector: "app-template",
+  templateUrl: "./template.component.html",
   styles: [`
     .ng-invalid.ng-touched:not(form) {
       border: 1px solid red;
@@ -12,19 +12,18 @@ import { NgForm } from '@angular/forms';
 })
 export class TemplateComponent implements OnInit {
 
-  usuario: Object = {
+  usuario: object = {
     nombre: null,
     apellido: null,
     email: null
-  }
+  };
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   guardar(forma: NgForm) {
-    console.log('Formulario posteado');
+    console.log("Formulario posteado");
     console.log("NgForm: ", forma);
     console.log("Valor forma: ", forma.value);
 
