6f68b6be3c6e9b167db647f112947f0793fb5adb
Roberto AMD <haschell@gmail.com>
2019-05-23 21:13:56 +0200

    Empezar clase 197

--- formularios/src/app/components/data/data.component.ts
+++ formularios/src/app/components/data/data.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent implements OnInit {
+export class DataComponent {
 
-  constructor() { }
+  forma: FormGroup;
 
-  ngOnInit() {
+  usuario: Object = {
+    nombrecompleto: {
+      nombre: "Roberto",
+      apellidos: "Ruiz"
+    },
+    correo: "roberto@gmail.com"
+  }
+
+  constructor() {
+    this.forma = new FormGroup({
+
+      nombrecompleto: new FormGroup({
+        nombre: new FormControl('', [Validators.required, Validators.minLength(3)] ),
+        apellidos: new FormControl('', Validators.required),
+      }),
+      correo: new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")] )
+     });
+  }
+
+  guardarCambios() {
+    console.log(this.forma.value);
+    console.log(this.forma);
   }
 
 }
