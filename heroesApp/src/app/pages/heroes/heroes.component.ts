af3409a50879a4aa9a2a7e72624b741704667faa
Roberto Ruiz <roberto.ruiz.martin@gmail.com>
2019-06-04 17:19:31 +0200

    Un video y examen faltan para acabar CRUD Firebase

---
+++ heroesApp/src/app/pages/heroes/heroes.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from "@angular/core";
+import { HeroesService } from "../../services/heroes.service";
+import { HeroeModel } from "../../models/heroe.model";
+import Swal from "sweetalert2";
+
+@Component({
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"]
+})
+export class HeroesComponent implements OnInit {
+  heroes: HeroeModel[] = [];
+
+  constructor(private heroesService: HeroesService) {}
+
+  ngOnInit() {
+    this.heroesService.getHeroes().subscribe(resp => (this.heroes = resp));
+  }
+
+  borrarHeroe(heroe: HeroeModel, i: number) {
+    Swal.fire({
+      title: "¿Seguro?",
+      text: `¿Está seguro de borrar a ${heroe.nombre}?`,
+      type: "question",
+      showConfirmButton: true,
+      showCancelButton: true
+    }).then(resp => {
+      if (resp.value) {
+        this.heroes.splice(i, 1);
+        this.heroesService.borrarHeroe(heroe.id).subscribe();
+      }
+    });
+  }
+}
