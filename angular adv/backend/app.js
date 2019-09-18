2ef167d3c8fab28efadb59acb0c0151a3d54b059
Roberto AMD <haschell@gmail.com>
2019-09-18 22:20:29 +0200

    Continuar en Seccion 11 - Clase 121 Get-Populate

--- angular adv/backend/app.js
+++ angular adv/backend/app.js
@@ -15,6 +15,8 @@ app.use(bodyParser.json());
 // Importar rutas
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
+var hospitalRoutes = require('./routes/hospital');
+var medicoRoutes = require('./routes/medico');
 var loginRoutes = require('./routes/login');
 
 // Conexion a la base de datos
@@ -24,6 +26,8 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 });
 
 // Rutas
+app.use('/medico', medicoRoutes);
+app.use('/hospital', hospitalRoutes);
 app.use('/usuario', usuarioRoutes);
 app.use('/login', loginRoutes);
 app.use('/', appRoutes);
