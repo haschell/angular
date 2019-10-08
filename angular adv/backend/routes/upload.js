f35cf7441a81faf9e5ee22724eb816fdfecb6a9c
Roberto AMD <haschell@gmail.com>
2019-10-08 21:59:39 +0200

    Continuar en Seccion 11 - Clase 131

--- angular adv/backend/routes/upload.js
+++ angular adv/backend/routes/upload.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var fileUpload = require('express-fileupload');
+var fs = require('fs');
 var app = express();
 
 //default options
 app.use(fileUpload());
+
+var Usuario = require('../models/usuario');
+var Medico = require('../models/medico');
+var Hospital = require('../models/hospital');
 
 // Rutas
 app.put('/:tipo/:id', (req, res, next) => {
@@ -33,7 +38,7 @@ app.put('/:tipo/:id', (req, res, next) => {
     // Obtener nombre del archivo
     var archivo = req.files.imagen;
     var nombreCortado = archivo.name.split('.');
-    var extensionArchivo = nombreCortad[nombreCortado.length - 1];
+    var extensionArchivo = nombreCortado[nombreCortado.length - 1];
 
     // Solo estas extensiones son aceptadas
     var extensionesValidas = ['png', 'jpg', 'gif', 'jpeg'];
@@ -76,16 +81,91 @@ app.put('/:tipo/:id', (req, res, next) => {
         }
 
 
-        // subirPorTipo(tipo, id, nombreArchivo, res);
-
-        res.status(200).json({
-            ok: true,
-            mensaje: 'Archivo movido',
-            extensionArchivo: extensionArchivo
-        });
-
-
+        subirPorTipo(tipo, id, nombreArchivo, res);
     });
 });
+
+function subirPorTipo(tipo, id, nombreArchivo, res) {
+
+    if (tipo === 'usuarios') {
+        Usuario.findById(id, (err, usuario) => {
+
+            if (!usuario) {
+                return res.status(400).json({
+                    ok: true,
+                    mensaje: 'Usuario no existe',
+                    usuario: { message: 'Usuario no existe' }
+                });
+            }
+
+            var pathViejo = './uploads/usuarios/' + usuario.img;
+            // Si existe, elimina la imagen anterior
+            if (fs.existsSync(pathViejo)) {
+                fs.unlink(pathViejo);
+            }
+            usuario.img = nombreArchivo;
+            usuario.save((err, usuarioActualizado) => {
+                usuarioActualizado.password = ':)';
+                return res.status(200).json({
+                    ok: true,
+                    mensaje: 'Imagen de usuario actualizada',
+                    usuario: usuarioActualizado
+                });
+            });
+        });
+    }
+    if (tipo === 'medicos') {
+        Medico.findById(id, (err, medico) => {
+
+            if (!medico) {
+                return res.status(400).json({
+                    ok: true,
+                    mensaje: 'Medico no existe',
+                    usuario: { message: 'Medico no existe' }
+                });
+            }
+
+            var pathViejo = './uploads/medicos/' + medico.img;
+            // Si existe, elimina la imagen anterior
+            if (fs.existsSync(pathViejo)) {
+                fs.unlink(pathViejo);
+            }
+            medico.img = nombreArchivo;
+            medico.save((err, medicoActualizado) => {
+                return res.status(200).json({
+                    ok: true,
+                    mensaje: 'Imagen de medico actualizada',
+                    usuario: medicoActualizado
+                });
+            });
+        });
+    }
+    if (tipo === 'hospitales') {
+        Hospital.findById(id, (err, hospital) => {
+
+            if (!hospital) {
+                return res.status(400).json({
+                    ok: true,
+                    mensaje: 'Hospital no existe',
+                    usuario: { message: 'Hospital no existe' }
+                });
+            }
+            var pathViejo = './uploads/hospitales/' + hospital.img;
+            // Si existe, elimina la imagen anterior
+            if (fs.existsSync(pathViejo)) {
+                fs.unlink(pathViejo);
+            }
+            hospital.img = nombreArchivo;
+            hospital.save((err, hospitalActualizado) => {
+                return res.status(200).json({
+                    ok: true,
+                    mensaje: 'Imagen de hospital actualizada',
+                    usuario: hospitalActualizado
+                });
+            });
+        });
+    }
+
+}
 
 module.exports = app;
