6c3063bd5546ee0a823be7f3296246e5934f5bfb
Roberto AMD <haschell@gmail.com>
2019-11-18 21:23:43 +0100

    Continuar Seccion 13 - Clase 161

--- angular adv/adminpro/src/app/services/usuario/usuario.service.ts
+++ angular adv/adminpro/src/app/services/usuario/usuario.service.ts
@@ -13,6 +13,21 @@ export class UsuarioService {
     console.log('Servicio de usuario listo!!');
   }
 
+  login(usuario: Usuario, recordar: boolean) {
+    if (recordar) {
+      localStorage.setItem('email', usuario.email);
+    } else {
+      localStorage.removeItem('email');
+    }
+    let url = URL_SERVICIOS + '/login';
+    return this.http.post(url, usuario).map((resp: any) => {
+      localStorage.setItem('id', resp.id);
+      localStorage.setItem('token', resp.token);
+      localStorage.setItem('usuario', JSON.stringify(resp.usuario));
+      return true;
+    });
+  }
+
   crearUsuario(usuario: Usuario) {
     let url = URL_SERVICIOS + '/usuario';
     return this.http.post( url, usuario ).map((resp: any)=>{
